c9f83a6b70179df5127b5cd632c009a93e920b10
anzev <anze.vavpetic@gmail.com>
2015-11-01 21:46:42 +0100

    Adds expression evaluator

--- parsing.c
+++ parsing.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <editline/readline.h>
 #include "mpc.h"
+
+
+double evaluateOp(char* op, double a, double b) {
+    double result;
+    if (strcmp(op, "+") == 0) {
+        result = a + b;
+    } else if (strcmp(op, "-") == 0) {
+        result = a - b;
+    } else if (strcmp(op, "*") == 0) {
+        result = a * b;
+    } else if (strcmp(op, "/") == 0) {
+        result = a / b;
+    }
+    return result;
+}
+
+
+double evaluate(mpc_ast_t* ast) {
+
+    if (strstr(ast->tag, "number")) {
+        return atof(ast->contents);
+    }
+
+    char* op = ast->children[1]->contents;
+
+    // Eval first argument
+    double a = evaluate(ast->children[2]);
+
+    int i;
+    for (i = 3; i < ast->children_num; i++) {
+        if (strstr(ast->children[i]->tag, "expression")) {
+            double b = evaluate(ast->children[i]);
+            a = evaluateOp(op, a, b);
+        }
+    }
+
+    return a;
+}
+
 
 int main(int argc, char** argv) {
     
@@ -12,7 +52,7 @@ int main(int argc, char** argv) {
     mpc_parser_t* Lispy      = mpc_new("lispy");
 
     mpca_lang(MPCA_LANG_DEFAULT,
-        "number     : /-?[0-9]+/ ;                                    \
+        "number     : /-?[0-9]+\\.?[0-9]*/ ;                          \
          operator   : '-' | '+' | '*' | '/' ;                         \
          expression : <number> | '(' <operator> <expression>+ ')' ;   \
          lispy      : /^/ <operator> <expression>+ /$/ ;              \
@@ -29,8 +69,8 @@ int main(int argc, char** argv) {
         // Parse user input
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r)) {
-            mpc_ast_print(r.output);
-            mpc_ast_delete(r.output);
+            mpc_ast_t* ast = r.output;
+            printf("= %.2f\n", evaluate(ast));
         } else {
             mpc_err_print(r.error);
             mpc_err_delete(r.error);
